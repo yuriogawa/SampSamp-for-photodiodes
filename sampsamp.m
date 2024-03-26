9667123a84ca70df7ca6a674aae412051a916137
Jaxon-Mitchell <jaxon.mitchell@flinders.edu.au>
2024-03-26 15:27:43 +1030

    Restructured file contents and organisation + began fixing UI

---
+++ sampsamp.m
@@ -0,0 +1,31 @@
+function sampsamp()
+
+% _____                       _____                       
+%/  ___|                     /  ___|                      
+%\ `--.  __ _ _ __ ___  _ __ \ `--.  __ _ _ __ ___  _ __  
+% `--. \/ _` | '_ ` _ \| '_ \ `--. \/ _` | '_ ` _ \| '_ \ 
+%/\__/ / (_| | | | | | | |_) /\__/ / (_| | | | | | | |_) |
+%\____/ \__,_|_| |_| |_| .__/\____/ \__,_|_| |_| |_| .__/ 
+%                      | |                         | |    
+%                      |_|                         |_|    
+%|------------------------------------------------------|
+%|   SampSamp 2.0 - Visual Stimulus user interface      |
+%|                                                      |
+%|    -Requires MatLab 2023 with Psychophysics toolbox  |
+%|    -Documentation and user's manual available        |
+%|                                                      |
+%|                                                      |
+%| info@flyfly.se             (c) Someone 2009          |
+%|------------------------------------------------------|
+%
+%               Why did the fly fly?
+%            Because the spider spied 'er!
+%
+%
+
+disp(' ');
+disp(' -SampSamp 2.0- ');
+
+addpath(cd, [cd '/' genpath('Functions')], [cd '/' genpath('Icons')]);
+clearRoot;
+main;
