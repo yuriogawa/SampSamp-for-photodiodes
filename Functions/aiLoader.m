13751418b2931296bc3973ba5ca0f0998ad98e6a
Jaxon-Mitchell <jaxon.mitchell@flinders.edu.au>
2024-04-09 14:07:09 +0930

    Removed references to timer callbacks to prepare for adding data available callbacks

--- Functions/aiLoader.m
+++ Functions/aiLoader.m
@@ -12,7 +12,6 @@ daqDevice = app.daqDevice;
 useTrig = app.trigOrNot.Value;
 
 sampleFrequency = app.freq.Value;
-timeout = app.sec.Value;
 
 
 % When you add channels you need the number of the
@@ -28,34 +27,14 @@ end
 
 %-------------------Specific properties for trigger------------------------
 
-if useTrig == 1    
-    % Start trigger is set on software trigger
-    set (daqDevice,'TriggerType','Software'); 
-    % unit[V] The TriggerConditionValue is the criteria for when thrigger signal invokes the trigger.
-    set (daqDevice,'TriggerConditionValue',samHandles.triggVal);
-    % Specifies the amount of data that is to be saved pre trigger in time unit [s] 
-    set (daqDevice,'TriggerDelay',-samHandles.delaySamples);
-    % Directs the software trigger to the channel that are supose to act as a trigger signal
-    set (daqDevice,'TriggerChannel',ch(1)); 
-end
-
-samplesPerTrig = sampleFrequency * timeout;
+% Configure DAQ ScansAvailableFcn callback function
+daqDevice.ScansAvailableFcn = @(src,event) scansAvailable_Callback(app, src, event);
 
 % Sets sample rate of ai will be rounded off to closes possible sampling rate 
 %(sample rate is limite by stepsize 1/250000 and has to be modulus 0 with it..)
-set(daqDevice,'SampleRate',sampleFrequency);
-
-% Determens longest possible sampling time before sampsamp considers the start 
-% trigger to be a false one and shots itself off
-set(daqDevice,'SamplesPerTrigger',samplesPerTrig);
+daqDevice.Rate = sampleFrequency;
 
 if useTrig == 1        
     % Sets the intervalls at which daqrealtimeplot_Callback is to be run
     set(daqDevice,'TimerPeriod',0.1);
 end
-
-% Function to be run within the interwall set by TriggerDelay
-set(daqDevice,'TimerFcn',@dRTPC)
-
-% Sets the function that is to be run when data is missed.
-set(daqDevice,'DataMissedFcn',@missedData);
