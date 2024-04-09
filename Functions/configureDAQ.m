ec5aaa95cedb290f8067570871b4a836c1413f20
Jaxon-Mitchell <jaxon.mitchell@flinders.edu.au>
2024-04-09 15:48:22 +0930

    Added beginning callback for when DAQ data is available

--- Functions/configureDAQ.m
+++ Functions/configureDAQ.m
@@ -1,16 +1,16 @@
-function [daqDevice] = aiLoader(app)
+function DAQ = configureDAQ(app)
 
 % Inputs to this function:
 % app = contains handles to all SampSamp properties
-% daqDevice = Handle to the DAQ object, allowing us to change our recording
-% settings
 % ai = Array corresponding to analogue inputs 0-7 and which to turn on
+% d = Handle to the DAQ object, allowing us to change our recording
+% settings
+% sampleFrequency = Frequency (Hz) at which DAQ device will sample at
+
 
 % Obtain variables from app properties
-ai = app.analogueInputChoice;
-daqDevice = app.daqDevice;
-useTrig = app.trigOrNot.Value;
-
+ai              = app.analogueInputChoice;
+d               = daq(app.daqType);
 sampleFrequency = app.freq.Value;
 
 
@@ -21,20 +21,17 @@ sampleFrequency = app.freq.Value;
 for i=0:7
     if ai(i+1) == 1
         % If selected, add a channel to be recorded to DAQ object
-        addinput(daqDevice, DAQID, i, "Voltage");
+        addinput(d, app.daqConnection, i, "Voltage");
     end
 end
 
 %-------------------Specific properties for trigger------------------------
 
 % Configure DAQ ScansAvailableFcn callback function
-daqDevice.ScansAvailableFcn = @(src,event) scansAvailable_Callback(app, src, event);
+d.ScansAvailableFcn = @(src,event) scansAvailable_Callback(app, src, event);
 
-% Sets sample rate of ai will be rounded off to closes possible sampling rate 
+% Sets sample rate of ai will be rounded off to closest possible sampling rate 
 %(sample rate is limite by stepsize 1/250000 and has to be modulus 0 with it..)
-daqDevice.Rate = sampleFrequency;
+d.Rate = sampleFrequency;
 
-if useTrig == 1        
-    % Sets the intervalls at which daqrealtimeplot_Callback is to be run
-    set(daqDevice,'TimerPeriod',0.1);
-end
+DAQ = d;
