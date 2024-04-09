3b85850fed4b2fd1d4f52e06f28258130b4bdb83
Jaxon-Mitchell <jaxon.mitchell@flinders.edu.au>
2024-04-09 16:53:02 +0930

    Waiting for data to buffer has been implemented

---
+++ Functions/isEnoughDataBuffered.m
@@ -0,0 +1,13 @@
+function results = isEnoughDataBuffered(timeBuffer, delaySamples)
+%isEnoughDataBuffered Checks whether buffering pre-trigger data is complete    
+
+    % If specified trigger delay is less than 0, need to check
+    % whether enough pre-trigger data is buffered so that a
+    % triggered capture can be requested
+
+    % Don't trust this, do alternative - Jaxon
+    results = 0;
+    if (size(timeBuffer,1) > delaySamples)
+        results = 1;
+    end
+           
